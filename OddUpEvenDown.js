8d8fced97d5085f24182167e25bcd80b3398bef2
subodh <subodhss23@gmail.com>
2019-09-24 11:18:46 -0700

    solution with ternary operator

--- OddUpEvenDown.js
+++ OddUpEvenDown.js
@@ -4,12 +4,20 @@
 function transform(arr){
     for (let i = 0; i < arr.length ; i++){
         if(arr[i] % 2 === 0){
-            arr[i] += 1;
-        } else{
             arr[i] -= 1;
+        } else{
+            arr[i] += 1;
         }
     }
     console.log(arr);
 }
 
+
+// solution using ternary operator
+function transform(arr){
+    for (let i = 0; i < arr.length; i++ ){
+        (arr[i] % 2 === 0 ? arr[i] -= 1 : arr[i] += 1)
+    }
+    console.log(arr);
+}
 transform([1,2,3,4]);
