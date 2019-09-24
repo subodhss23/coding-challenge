3b672c9fda60c0a45470d8e34db7e70d6316c08b
subodh <subodhss23@gmail.com>
2019-09-24 16:26:00 -0700

    done

---
+++ multiplyByLength.js
@@ -0,0 +1,10 @@
+// create a function to multiply all values in an array by the
+// amount of values in that array.
+
+
+function MultiplyByLength(arr){
+    let newarr =arr.map((x) => x * arr.length);
+    console.log(newarr);
+}
+
+MultiplyByLength([1,2,3,4]);
