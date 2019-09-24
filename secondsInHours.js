1b5d223a8a8d931f4ba312443eb14d9a5d84612c
subodh <subodhss23@gmail.com>
2019-09-24 12:33:05 -0700

    easy

---
+++ secondsInHours.js
@@ -0,0 +1,10 @@
+// write a functio that converst hours into seconds
+
+
+function howManySeconds(hours){
+    let seconds = 60*60*hours;
+    console.log(seconds);
+}
+
+howManySeconds(1);
+howManySeconds(10);
