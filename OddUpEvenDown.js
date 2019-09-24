b185190e1b49459f33b3ed233a56f324f86b1c06
subodh <subodhss23@gmail.com>
2019-09-24 11:15:32 -0700

    bruteforce while loop

---
+++ OddUpEvenDown.js
@@ -0,0 +1,15 @@
+// create a function that goes through the array, incrementing(+1)
+// for each odd number and decrementing(-1) for each even number
+
+function transform(arr){
+    for (let i = 0; i < arr.length ; i++){
+        if(arr[i] % 2 === 0){
+            arr[i] += 1;
+        } else{
+            arr[i] -= 1;
+        }
+    }
+    console.log(arr);
+}
+
+transform([1,2,3,4]);
