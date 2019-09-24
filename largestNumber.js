04a58060142bdbc67a53b200a63a6d645ea02bc1
subodh <subodhss23@gmail.com>
2019-09-24 14:31:12 -0700

    spread operator

---
+++ largestNumber.js
@@ -0,0 +1,10 @@
+// create a function that takes an array of numbers. Return the
+// largest nubmer in the array.
+
+
+function findLargestNum(arr){
+    console.log(Math.max(...arr));
+  
+}
+
+findLargestNum([1,2,3,44,5]);
