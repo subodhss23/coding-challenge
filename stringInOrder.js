1e74103a9b8c58d46fceb7a9e1101707ed10abc1
subodh <subodhss23@gmail.com>
2019-09-24 14:05:39 -0700

    .split() .join() .sort()

---
+++ stringInOrder.js
@@ -0,0 +1,26 @@
+// create a function that takes a string and returns true or false,
+// depending on whether the characters are in order or not.
+
+
+
+function isInOrder(str){
+    let newarr = str.split('');
+    console.log(newarr.sort());
+    let sortedArr = newarr.join('');
+    console.log(sortedArr);
+
+    if(str === sortedArr){
+        console.log('true');
+    } else {
+        console.log('false');
+    }
+}
+
+isInOrder("abcdef")
+
+if(str === sortedArr){
+    return true;
+} else {
+    return false;
+}
+}
