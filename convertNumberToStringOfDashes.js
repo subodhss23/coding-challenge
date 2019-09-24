4199b2573f361f3630133e80b33487069c31adb0
subodh <subodhss23@gmail.com>
2019-09-24 11:08:38 -0700

    multiple solutions

--- convertNumberToStringOfDashes.js
+++ convertNumberToStringOfDashes.js
@@ -1,11 +1,26 @@
 // create a function that takes a number (from 1-60) and 
 // returns a corresponding string of hyphens
 
+
+// 1. solution using in built .repeat method
 function Go(num){
     let h = '-';
     let string = h.repeat(num);
     console.log(string);
-    console.log(typeof(string));
 }
 
-Go(22);
+// 2. solution using repeat method as well
+function Go(num){
+    console.log ('-'.repeat(num));
+}
+
+// 3. solution using for loop 
+function Go(num){
+    str = '';
+    for(let i = 0; i < num; i++){
+        str += '-';
+    }
+    console.log(str);
+}
+
+Go(5);
