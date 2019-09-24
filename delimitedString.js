6dbfc1b3128fb531df5691de2a451eb76802c7d5
subodh <subodhss23@gmail.com>
2019-09-24 12:56:45 -0700

    deliminted

--- delimitedString.js
+++ delimitedString.js
@@ -1,18 +1,26 @@
 // write a function that turns a comma-delimited list into an 
 //array of strings.
 
+// function toArray(str) {
+// 	if (str === "") {
+//         console.log([]);
+// 	} 
+// 	result = str.split(", ");
+// 	console.log(result);
+// }
+
+
 function toArray(str){
-    if(str.length >= 0){
+    if(str){
         console.log(str.split(', '));
     } else {
-        console.log([]);
+        console.log('null');
     }
 }
-
-
 // solution using ternary operator
-function toArray(str){
-    console.log(str.length > 0 ? str.split(', ') : []);
-}
+// function toArray(str){
+//     console.log(str.length > 0 ? str.split(', ') : []);
+// }
 toArray("watermelon, raspberry, orange");
+toArray([]);
 
