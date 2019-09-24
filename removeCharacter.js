885f879fbc2f85b408128f7b621ccc680673586c
subodh <subodhss23@gmail.com>
2019-09-24 13:08:02 -0700

    str.slice()

---
+++ removeCharacter.js
@@ -0,0 +1,21 @@
+// Create a function that removes the first and last characters
+// from a string.
+
+
+//Note
+// if the string is 2 or fewer character long, return the string
+
+function removeFirstLast(str){
+    if(str.length < 3){
+        return str;
+    }else{
+       let newstr = str.slice(1, str.length - 1);
+       console.log(newstr);
+    //    console.log(str[str.length - 1]);
+    }
+}
+
+let string = "string";
+console.log(string.length);
+
+removeFirstLast('whatisthat?');
