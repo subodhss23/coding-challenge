427a29bf42f8326020b03c3ad27967432053df68
subodh <subodhss23@gmail.com>
2019-09-24 11:59:56 -0700

    for and if loops

---
+++ isSamWithFrodo.js
@@ -0,0 +1,17 @@
+// Sam and Frodo need to be close. If they are side by side
+// in the array, your function should return true. If there
+// is a name between them, return false.
+
+function middleEarth(arr){
+    for(let i = 0; i < arr.length - 1 ; i++){
+        if(arr[i] === "Sam" && arr[i+1] === "Frodo" || arr[i] === "Frodo" && arr[i+1] === "Sam"){
+            console.log('true');
+        } 
+    }
+ console.log('false'); 
+}
+
+middleEarth(["Sam", "Frodo", "Gandalf"]);
+// middleEarth(["Frodo", "Saruman", "Sam"]);
+// middleEarth(["Orc", "Sam", "Frodo", "Legolas"])
+
