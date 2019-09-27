79f94169853d6f2c907b3d5fbdecb4a4f2a8ce1d
subodh <subodhss23@gmail.com>
2019-09-27 13:49:19 -0700

    min and spread operator

---
+++ smallestNumber.js
@@ -0,0 +1,8 @@
+// create a function thta takes an array of nubmers and
+// returns the smallest number in the set
+
+function findSmallestNum(arr){
+    console.log(Math.min(...arr));
+}
+
+findSmallestNum([34, 15, 88, 2])
