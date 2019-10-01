d7dd382dcc891cef9bf23b28859180f93e2c0d58
subodh <subodhss23@gmail.com>
2019-09-30 23:20:31 -0700

    endsWith()

---
+++ matchSecondString.js
@@ -0,0 +1,9 @@
+// create a function that takes two strings and returns true if
+// the first string ends with the second string; otherwise return false;
+
+function checkEnding(str1, str2){
+     console.log(str1.endsWith(str2));
+}
+
+checkEnding("abc", "bc");
+checkEnding("abc", "d");
